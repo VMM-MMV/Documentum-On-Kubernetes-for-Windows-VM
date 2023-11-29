af74cd65715ebfe500a0acccf24ee2ab07fae20a
VMM-MMV <121231490+VMM-MMV@users.noreply.github.com>
2023-11-29 10:08:08 +0200

    first commit

---
+++ docker_pull.cmd
@@ -0,0 +1,11 @@
+docker pull postgres:15.1
+docker tag postgres:15.1 registry.opentext.com/cs/pg:15.1
+docker pull registry.opentext.com/dctm-d2pp-rest-ol:23.2
+docker pull registry.opentext.com/dctm-tomcat:23.2
+docker pull registry.opentext.com/dctm-server:23.2
+docker pull registry.opentext.com/otds-server:23.1.1
+docker pull registry.opentext.com/dctm-rest:23.2
+docker pull registry.opentext.com/dctm-admin:23.2
+docker pull registry.opentext.com/dctm-content-connect:23.2
+docker pull registry.opentext.com/dctm-content-connect-dbinit:23.2
+docker logout
